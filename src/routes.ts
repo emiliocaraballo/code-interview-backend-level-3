794bfcad2ebfce5e282424046773a2f3b340ce93
Emilio Caraballo <emiliocaraballo9810@gmail.com>
2025-01-08 17:19:44 -0500

    feat(server): configure initial Hapi.js server

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,21 @@
-import { Server } from "@hapi/hapi"
+import { Server } from "@hapi/hapi";
+import { itemRoutes } from "src/modules/item.router";
 
 export const defineRoutes = (server: Server) => {
-    server.route({
-        method: 'GET',
-        path: '/ping',
-        handler: async (request, h) => {
-            return {
-                ok: true
-            }
-        }
-    })  
-}
+  itemRoutes(server);
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return { message: "¡Hola desde Hapi.js con TypeScript!" };
+    },
+  });
+
+  server.route({
+    method: "GET",
+    path: "/ping",
+    handler: (request, h) => {
+      return { ok: true };
+    },
+  });
+};
