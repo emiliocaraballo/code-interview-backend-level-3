470299e0b76ba9cacc6a1ac1216fbd8009a600f3
Emilio Caraballo <emiliocaraballo9810@gmail.com>
2025-01-08 17:15:29 -0500

    feat: config jest and package from proyect

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-    transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
-    },
-   }
-   
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/e2e/**/*.test.ts'], // Define dónde están los tests
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json', // Apunta a tu archivo tsconfig.json
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1' // Mapeo para rutas absolutas
+  },
+  forceExit: true,
+};
